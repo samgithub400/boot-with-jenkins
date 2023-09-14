c542a264b977e35ea406bd859f5f99f3642c7896
Tohit Shaikh <shaikhtohit95@gmail.com>
2023-09-14 08:09:01 +0530

    Initial commit

---
+++ src/main/java/com/ios/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.ios.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+      return "Welcome...! Test Successfully....!";
+    }
+}
