a0b8c993f4a1f2f479a38422cdede864544d5360
Tohit Shaikh <shaikhtohit95@gmail.com>
2023-09-14 22:29:14 +0530

    creating war file of spring boot app

--- src/main/java/com/ios/BootWithJenkinsApplication.java
+++ src/main/java/com/ios/BootWithJenkinsApplication.java
@@ -2,12 +2,22 @@ package com.ios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BootWithJenkinsApplication {
+public class BootWithJenkinsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootWithJenkinsApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		return builder.sources (BootWithJenkinsApplication.class);
+	}
+
+
 
 }
